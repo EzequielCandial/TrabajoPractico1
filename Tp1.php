16ec5fcf638d95b7a6b21d0c278f1d3e2cff6900
EzequielCandial <141389181+EzequielCandial@users.noreply.github.com>
2023-09-12 21:41:04 -0300

    Trabajo Practico 1 Candial, Ezequiel

---
+++ Tp1.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="  " dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+<?php
+echo '1. Hola Mundo'. "<br><br>";
+
+
+$saludo = "2. Hola Mundo";
+echo $saludo. "<br><br>";
+
+
+echo "3.". "<br>";
+
+$Punto1 = 15;
+$Punto2 = 5;
+
+$total = $Punto1-$Punto2;
+echo $total. "<br>";
+
+$total = $Punto1+$Punto2;
+echo $total. "<br>";
+
+
+ $total = $Punto1*$Punto2;
+ echo $total. "<br>";
+
+
+ $total = $Punto1/$Punto2;
+ echo $total. "<br>";
+
+ $total = $Punto1%$Punto2;
+ echo $total. "<br><br>";
+
+
+ echo "4.". "<br>";
+
+$Grados = 20;
+$Formula = 9/5;
+$F = 32;
+$total = $Grados*$Formula+$F;
+echo $total. "<br><br>";
+
+
+echo "5.";
+
+echo "a.". "<br>";
+
+  $ancho = 12;
+  $largo = 18;
+  $area = $ancho*$largo;
+  echo $area. "<br>";
+
+$base = 12;
+$altura = 18;
+$formula = 2;
+$perimetro = $formula*($base+$altura);
+echo $perimetro. "<br><br>";
+
+
+echo "b.". "<br>";
+$Pi = 3.14;
+$Radio = 30;
+$Doble = 2;
+$Area = $Pi*$Radio*$Radio. "<br>";
+echo $Area;
+$Perimetro = $Doble*$Pi*$Radio;
+
+echo $Perimetro;
+?>
+
+
+  </body>
+</html>
